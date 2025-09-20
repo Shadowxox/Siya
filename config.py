db5932f417831052a161c38f67e6f5cffdb7e82c
Shadowxox <shadowx0x12@gmail.com>
2025-09-20 08:15:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", None))
-OWNER_USERNAME = os.getenv("OWNER_USERNAME", "WTF_WhyMeeh")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "ShrutixMusicBot")
+OWNER_USERNAME = os.getenv("OWNER_USERNAME", "Morvainn")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "SiyaMusicProbot")
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # 🛠️ Database & Deployment Configs
@@ -55,20 +55,20 @@ HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 # 🔄 Git & Update Settings
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/NoxxOP/ShrutiMusic")
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/Shadowxox/Siya")
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
-GIT_TOKEN = os.getenv("GIT_TOKEN", None)
+GIT_TOKEN = os.getenv("GIT_TOKEN", "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")
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # 🔗 Support Links
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/ShrutiBots")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/ShrutiBotSupport")
-INSTAGRAM = os.getenv("INSTAGRAM", "https://instagram.com/yaduwanshi_nand")
-YOUTUBE = os.getenv("YOUTUBE", "https://youtube.com/@NandEditz")
-GITHUB = os.getenv("GITHUB", "https://github.com/NoxxOP")
-DONATE = os.getenv("DONATE", "https://t.me/ShrutiBots/91")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/XDUPDATE")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/LOVE_DOSE_CHAT")
+INSTAGRAM = os.getenv("INSTAGRAM", "https://instagram.com")
+YOUTUBE = os.getenv("YOUTUBE", "https://youtube.com")
+GITHUB = os.getenv("GITHUB", "https://github.com/Shadow737hub")
+DONATE = os.getenv("DONATE", "https://t.me/Kavya_Bots")
 PRIVACY_LINK = os.getenv("PRIVACY_LINK", "https://graph.org/Privacy-Policy-05-01-30")
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -112,18 +112,18 @@ AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 # 🖼️ Image URLs (Can be customized)
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/7q8bfg.jpg")
-PING_IMG_URL = os.getenv("PING_IMG_URL", "https://files.catbox.moe/eehxb4.jpg")
-PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/eehxb4.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
+START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/ty7k49.jpg")
+PING_IMG_URL = os.getenv("PING_IMG_URL", "https://files.catbox.moe/t7emk1.jpg")
+PLAYLIST_IMG_URL = "https://files.catbox.moe/smcn2c.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/694f9h.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/694f9h.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/694f9h.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/694f9h.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/694f9h.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/694f9h.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/694f9h.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/694f9h.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/694f9h.jpg"
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # 🔐 User & Bot State Stores
